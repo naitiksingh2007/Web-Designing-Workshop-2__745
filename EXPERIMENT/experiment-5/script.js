d6f36163eaf4d8030cee7b9ea31b5feeaffbaac3
naitiksingh2007 <ysdpnaitik170@gmail.com>
2026-04-13 14:58:55 +0530

    mid term one experiment

---
+++ EXPERIMENT/experiment-5/script.js
@@ -0,0 +1,8 @@
+let employees = [];
+function addEmployee(){
+    let emp = {
+        name:document.getElementById("name")
+        id: document.getElementById("id").value
+        salary: Number(document.getElementById())
+    };
+}
